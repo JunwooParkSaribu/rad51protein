3ec733ce136403530f8df79a185bb3b9f12bd65f
JunwooParkSaribu <jpark@core-login2.cluster.france-bioinformatique.fr>
2023-05-18 13:22:16 +0200

    cpu core 16

--- submit_gpu.sh
+++ submit_gpu.sh
@@ -4,7 +4,7 @@
 #SBATCH --account=histoneclf
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:7g.40gb:1
-#SBATCH --cpus-per-task=12
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=64GB
 
 module load cudatoolkit/11.6.0
